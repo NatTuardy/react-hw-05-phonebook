8b66e97436941ebd508b9caf13fda592e0bcbedb
Your Name <you@example.com>
2020-05-20 15:21:34 +0300

    Updates

---
+++ precache-manifest.6cb9d0aec62c2d2328f91d749786ed9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "175dda31b4383eee365cedbc4fc5e5cb",
+    "url": "/react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "3788d82700e95b2cf1bc",
+    "url": "/react-hw-05-phonebook/static/css/main.ec49a2b9.chunk.css"
+  },
+  {
+    "revision": "0c4aab651f417bde9b96",
+    "url": "/react-hw-05-phonebook/static/js/2.67e4db66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-05-phonebook/static/js/2.67e4db66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3788d82700e95b2cf1bc",
+    "url": "/react-hw-05-phonebook/static/js/main.eff90ba4.chunk.js"
+  },
+  {
+    "revision": "e1809e1f38bda9c2cc3b",
+    "url": "/react-hw-05-phonebook/static/js/runtime-main.54ea0e71.js"
+  }
+]);
